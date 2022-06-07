67a578081124debd414adbfae94d9ef68e3e5816
Tommy Spurlock <tspurloc@gmail.com>
2022-06-07 13:18:02 -0400

    book-1 chapter3 loops and push review.

---
+++ main.js
@@ -0,0 +1,40 @@
+/// looooooooooooops
+
+
+const originals = [ "Original paper 1", "Original paper 2", "Original paper 3" ]
+
+originals.push("Original paper 4")
+originals.push("Original paper 5")
+
+// console.log(originals)
+
+// for(let i = 0; i < originals.length; i++) {
+//     console.log(originals[i])
+// }
+
+
+//    set variable            | name of array
+// for(let originalPaperNumber of originals) {
+//     console.log(originalPaperNumber)
+// }
+//Instructions
+
+// Your task is to iterate the array containing the chunks of clay and after your code is done, the toFireInKiln array should contain the string value "Clay coffee mug" for every string in the clay array.
+
+
+// const clay = [ "Clay", "Glass", "Mud", "Heart" ]  //array 1
+// const toFireInKiln = []      ///array 2
+
+// for (const taco of clay ) {
+//    const iAmAVariable = `${taco} coffee mug`
+//     toFireInKiln.push(iAmAVariable)
+
+// }
+
+// console.log(toFireInKiln)
+
+
+const happy = [4, 18, 47]
+console.log(happy)
+happy.push(24, 26)
+console.log(happy)
